4d34f3a4f1fabe400840477a6b6d2511fd5f28ac
Deawna Alfonsi <Deawna@D-Fit.com>
2019-04-10 13:03:33 -0400

    working on login component

--- flashcards/src/components/Login.vue
+++ flashcards/src/components/Login.vue
@@ -10,10 +10,14 @@
         <li id="passwordField">
             <input type="text" id="login-password" placeholder="Enter your password" v-model.trim="password"/>
         </li>
-        <button id="submitLoginButton" v-on:click="submitLogin(vm)">Submit</button>
+        <button id="submitLoginButton" v-on:click="submitLogin">Submit</button>
         <button id="cancelLoginButton" v-on:click.prevent="showLoginForm = false">Cancel</button>
-
         </ul>
+
+        <p id="loginSuccessful" v-if="showSuccessMsg===true">Welcome {{singleUser.FirstName}} {{singleUser.LastName}}!</p>
+        <p id="loginFailed" v-if="showFailMsg===true">Email and/or password are not a match.</p>
+
+
     </div>
 </template>
 
@@ -29,8 +33,11 @@ export default {
             showLoginForm: false, 
             users: [],
             //example:  "http://localhost:5000/api/cards",
-            apiURL: "YOUR_REST_API_URL",
-        };
+            apiURL: "https://localhost:44337/api/flashycards",
+            showSuccessMsg: false, 
+            showFailMsg: false, 
+            singleUser: {}
+            };
     }, 
 
     methods: {
@@ -38,36 +45,45 @@ export default {
         //Finish Login Section
         submitLogin(input)
         {
-            // use fetch to bring back all users from the database         
-            fetch(this.apiURL)
+            // use fetch to this user from the database         
+            fetch(this.apiURL + '/' + this.email + '/' + this.password)
                 .then(response => {
                     return response.json();
                 })
             //assign the user objects from the DB to the users array defined in this component
-                .then(users => {
-                    this.users = users;
+                .then(user => {
+                    this.singleUser = user;
                 });   
 
             this.showLoginForm = false;
             
             //filter thru users array and return the user with matching email
+            /*
             let confirmedUser = this.users.filter((user) => {
                 return input.email === user.Username;
-            });
+            }); */
 
             //if a confirmed user is returned, emit the confirmedUser object so that the next component
             //can obtain this info
-            if (confirmedUser != null){
-                this.$emit('confirmedUser', confirmedUser);
-
+            if (this.singleUser != null || this.singleUser != {}){
+                this.showSuccessMsg = true;
+                this.$emit('confirmedUser', this.singleUser);
+                console.log("This worked.");
+               }
             //otherwise emit empty object titled noUserFound
-            } else {
-                this.$emit('noUserFound', confirmedUser); 
+             else {
+               this.showFailMsg = true;
+                this.$emit('noUserFound', this.singleUser); 
+                console.log("This DID NOT work!");
             }
+
             
+
             //clear the email and password fields
             this.email='';
             this.password = '';
+            
+            
         }
     //Once it's determined which component will pick up the emit from this function, then have to bind that in the App.vue file
     //Ex:  <componentName v-bind:elementName="confirmedUser">  or <componentName v-bind:elementName="noUserFound">
