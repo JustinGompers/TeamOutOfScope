585c30b4b41135a8e393829615876891c6b753f9
Justin Gompers <justinmgompers@aol.com>
2019-04-11 12:18:10 -0400

    front page fun

--- flashcards/src/App.vue
+++ flashcards/src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <div id="app">
-    <h1 id="title">Flashy Cards</h1>
-    <img id="logo" src="./assets/flashed-clipart-running.gif">
-    <login></login>
-    <registration></registration>
-    <card></card>
-    <deck></deck>
+    <fixed-header>
+      <div id='header'>
+        <img id="logo" src="./assets/flashed-clipart-running.gif">
+        <h1 id="title">Flashy Cards</h1>
+        <img id="logo" src="./assets/reverse-flash.gif">
+      </div>
+    </fixed-header>
+    <div class='content'>
+      <login></login>
+      <registration></registration>
+      <card></card>
+      <deck></deck>
+    </div>
   </div>
 
 
@@ -17,10 +24,12 @@ import Card from './components/Card.vue'
 import Login from './components/Login.vue'
 import Registration from './components/Registration.vue'
 import Deck from './components/Deck.vue'
+import FixedHeader from 'vue-fixed-header'
 
 export default {
   name: 'app',
   components: {
+    FixedHeader,
     Card,
     Login,
     Registration, 
@@ -36,13 +45,26 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
   background: lightblue;
 }
+#header.vue-fixed-header--isFixed {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100vw;
+}
+#header{
+  display: flex;
+  justify-content: center;
+  background: black;
+  color:white;
+  
+}
 #logo{
-  width: 25%;
+  width: 5vw;
+  height: 5vw;
 }
 #title{
-  font-size: 600%;
+  font-size: 3vw;
 }
 </style>
