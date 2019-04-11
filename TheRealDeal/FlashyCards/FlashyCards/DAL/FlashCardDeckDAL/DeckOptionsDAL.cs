b68c3f36670842d988b074f83e3e35c385a67d8c
Ryan Rugh <ryanrugh@gmail.com>
2019-04-11 15:38:55 -0400

    Backend stuff

---
+++ TheRealDeal/FlashyCards/FlashyCards/DAL/FlashCardDeckDAL/DeckOptionsDAL.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlashyCards.Model.UserInfoModels;
+
+namespace FlashyCards.DAL.FlashCardDeckDAL
+{
+    public class DeckOptionsDAL : IDeckOptionsDAL
+    {
+        private string connectionString;
+        private const string SQL_GetCategories = "Select * FROM Category order by asc;";
+
+        public DeckOptionsDAL(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        //public Category GetCategoryList()
+        //{
+        //    List<Category> CategoryList = new List<Category>();
+
+        //    try
+        //    {
+        //        using(SqlConnection)
+
+        //    }
+        //    catch (Exception)
+        //    {
+
+        //        throw;
+        //    }
+        }
+    }
+}
