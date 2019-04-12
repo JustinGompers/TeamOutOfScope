57954571cdbd56c6fd9d46e421495b3468e41a6b
wesley paioff <wesleypaioff@gmail.com>
2019-04-12 10:27:59 -0400

    commented out code

--- TheRealDeal/FlashyCards/FlashyCards/DAL/FlashCardDAL/CardOptionsDAL.cs
+++ TheRealDeal/FlashyCards/FlashyCards/DAL/FlashCardDAL/CardOptionsDAL.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using FlashyCards.Model.FlashCardModel;
 
 namespace FlashyCards.DAL.FlashCardDAL
 {
@@ -9,10 +11,41 @@ namespace FlashyCards.DAL.FlashCardDAL
     {
         private string connectionString;
         private const string SQL_GetCards = "Select * FROM Card order by asc;";
+
+        public CardOptionsDAL(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+        public List<Cards> getCardInfo()
+        {
+            List<Cards> CardsList = new List<Cards>();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(SQL_GetCards, conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        FlashCard tempCard = new FlashCard();
+                        tempCard.question = Convert.ToString(reader["Name"]);
+                        tempCard.Card_id = Convert.ToInt32(reader["Category_id"]);
+
+                        CardsList.Add(tempCard);
+                    }
+                }
+
+            }
+            catch (SqlException)
+            {
+
+                throw;
+            }
+            return CategoryList;
+        }
+
     }
 
-    public CardOptionsDAL(string connectionString)
-    {
-        this.connectionString = connectionString;
-    }
+    
 }
--- TheRealDeal/FlashyCards/FlashyCards/Model/FlashCardModel/FlashCard.cs
+++ TheRealDeal/FlashyCards/FlashyCards/Model/FlashCardModel/FlashCard.cs
@@ -7,5 +7,9 @@ namespace FlashyCards.Model.FlashCardModel
 {
     public class FlashCard
     {
+        public int userID { get; set; }
+        public string question { get; set; }
+        public string answer { get; set; }
+        public string images { get; set; }
     }
 }
