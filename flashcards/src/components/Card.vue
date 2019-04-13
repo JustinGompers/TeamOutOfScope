d87252c15b918f7ad9943ff65573e1a5d292e04e
wesley paioff <wesleypaioff@gmail.com>
2019-04-12 20:50:19 -0400

    cardzzzz

--- flashcards/src/components/Card.vue
+++ flashcards/src/components/Card.vue
@@ -1,6 +1,5 @@
 <template>
-    <div class= "cardSection"> 
-        <h2>This is where the card section will go</h2>   
+    <div class= "cardSection">   
       <button id="CardButton" v-on:click.prevent="showCardForm = true" v-if="showCardForm== false">Create Your Card</button>
       
       <form id="formCard" v-if="showCardForm" >
@@ -19,7 +18,7 @@
         
       </form>
       </div>
-    </div>
+    
 </template> 
         
     
@@ -71,95 +70,12 @@ export default {
         }
     }
     },
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-    //borrowed from TodoList.vue file from Building REST APIs tutorial
-    //not entirely sure yet how it might be used--assign what comes back from FlashCardsDB to the local cards variable?
-    created() {
-    fetch(this.apiURL)
-      .then(response => {
-        return response.json();
-      })
-      .then(cards => {
-        this.cards = cards;
-      })
-      .catch(err => console.log(err));
-    },
-
-    //borrowed from TodoList.vue file from Building REST APIs tutorial
-    //may still need tweaked to our specific application
-    addCard() {
-      const newCard = document.getElementById("new-card");
-      //we will be accepting properties of a card object (question, answer & maybe image too)
-      //need some way to assign multiple properties... is this enough to do it?
-      const card = newCard.value;
-      const maxid = Math.max(...this.cards.map(card => card.id));
-      //const card = { id: maxid + 1 };
-
-      // use fetch to create a new card in our FlashCardsDB.
-      fetch(this.apiURL, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json; charset=utf-8"
-        },
-        body: JSON.stringify(card)
-      })
-        .then(response => {
-          if (response.ok) {
-            // if our post was successful add the card to our cards array so the user can see it
-            // the next time this page is refreshed the new card will be pulled from the server.
-            this.cards.push(card);
-          }
-        })
-        .catch(err => console.error(err));
-
-      // clear the value so the user can type another task
-      newCard.value = "";
-    },
-
-    showNewCardForm() {
-      console.log("Add Card...");
-      // the list-item that contains the input box
-      const form = document.getElementById("newCardQuestion");
-      // the new todo form field
-      const input = document.getElementById("new-question");
-      // the list-item is hidden by default
-      form.removeAttribute("style");
-      // set the focus to the input box so the user can start typing a new card
-      const form2 = document.getElementById("newCardAnswer");
-      // the new todo form field
-      const input2 = document.getElementById("new-answer");
-      // the list-item is hidden by default
-      form.removeAttribute("style");
-      // set the focus to the input box so the user can start typing a new card
-      input.focus();
-    }
-    */
-
-
-
 }
+
 </script>
+
+<style>
+#modal-header{
+    text-align: center;
+}
+</style>
