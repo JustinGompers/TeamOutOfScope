28ff26fe997be7cddb573356beff78b3d843f463
wesley paioff <wesleypaioff@gmail.com>
2019-04-14 18:09:09 -0400

    card.vur

--- flashcards/src/components/Card.vue
+++ flashcards/src/components/Card.vue
@@ -1,20 +1,24 @@
 <template>
-    <div class= "cardSection">   
+    <div class= "CardSection">   
       <button id="CardButton" v-on:click.prevent="showCardForm = true" v-if="showCardForm== false">Create Your Card</button>
       
       <form id="formCard" v-if="showCardForm" >
 
         <div>
-        <label>Question:</label>
-            <input type="text" id="question" placeholder="Enter a question" v-model="question" />
+        <input type="text" id="question" placeholder="Enter a question" v-model="question" />
         </div>
         <div>
-          <label>Answer: </label>
-            <input type="text" id="answer" placeholder="Enter the answer" v-model="answer" />
+        <input type="text" id="answer" placeholder="Enter the answer" v-model="answer" />
+        </div>
+        <div>
+        <input type="text" id="image" placeholder="Enter an image url" v-model="image" />
+        </div>
+        <div>
+        <input type="text" id="tags" placeholder="Enter tags" v-model="tags" />
         </div>
         
-        <button id="submitQuestionButton" v-on:click="submitCard">Submit</button>
-        <button id="cancelQuestionButton" v-on:click.prevent="showCardForm = false">Cancel</button>
+        <button id="SubmitButton" v-on:click="submitCard">Submit</button>
+        <button id="CancelButton" v-on:click.prevent="showCardForm = false">Cancel</button>
         
       </form>
       </div>
@@ -26,21 +30,23 @@
 export default {
     name: 'Card',   
 
-    props: {
-        id: Number,
-        question: String,
-        answer: String,
-        image: String
-    },   
+    // props: {
+    //     id: Number,
+    //     question: String,
+    //     answer: String,
+    //     image: String
+    // },   
 
     data() {
         return {
 
           cards: [],
           showCardForm: false,
-          apiURL: "https://localhost:44337/api/flashycards",
+          apiURL: "https://localhost:44337/api/Card",
           question: '',
           answer: '',
+          image: '',
+          tags: '',
             
         };  
     },
@@ -63,6 +69,8 @@ export default {
             });
             this.question = '';
             this.answer = '';
+            this.image = '';
+            this.tags = '';
             this.showCardForm = false;
             alert('Your card has been submitted!');
         }else{
@@ -74,8 +82,29 @@ export default {
 
 </script>
 
+
 <style>
-#modal-header{
-    text-align: center;
+
+#CardButton{
+  border: 1px solid rgb(207, 93, 0);
+  border-radius: 15px;
+  box-shadow: 2px 2px #888888;
 }
+#SubmitButton{
+  border: 1px solid rgb(207, 93, 0);
+  border-radius: 15px;
+  box-shadow: 2px 2px #888888;
+}
+#CancelButton{
+  border: 1px solid rgb(207, 93, 0);
+  border-radius: 15px;
+  box-shadow: 2px 2px #888888;
+}
+
+
+input {
+  display: inline-block;
+  
+}
+
 </style>
