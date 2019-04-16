9324b61d2498ed975aee750fafcc0ef73691f2a3
wesley paioff <wesleypaioff@gmail.com>
2019-04-16 13:00:46 -0400

    seacrhy

--- flashcards/src/components/SearchCard.vue
+++ flashcards/src/components/SearchCard.vue
@@ -1,5 +1,5 @@
 <template>
-<div class= "SearchCardSection">   
+    <div class= "SearchCardSection">   
       <a id="SearchButton" v-on:click.prevent="show()">Search Flash Cards</a>
       <modal id="Form" name="SearchCard" :width="600" :height="400">
         <div id="modal-header">
@@ -10,34 +10,74 @@
           <input type="hidden" id="CardId" value="1" name="cardId" />
         <div>
           <label>Search: </label>
-        <input type="text" v-validate="'required'" id="search" placeholder="Enter a tag" name="search" v-model="question" />
+        <input type="text" v-validate="'required'" id="search" placeholder="Enter a tag" name="search" v-model="search" />
         </div>
          <button id="SubmitButton">Submit</button>
-      <button id="CancelButton" v-on:click.prevent="hide()">Cancel</button>
+        <button id="CancelButton" v-on:click.prevent="hide()">Cancel</button>
       </form>
       </div>
      
-      </modal>
-        
-
-      </div>
-      
+    </modal>
+  
 </div>
 
 </template>
 
 <script>
 export default {
-    name: 'Card',
+    name: 'SearchCard',
 
     data() {
         return {
 
           cards: [],
+          showFormSearch: false,
           apiURL: "https://localhost:44337/api/SearchCard",
-          Search: '',
+          search: '',
             
+        };
+    },
+    methods: {
+    Button() {
+         this.$validator.validateAll().then((result) => {
+             if (result) {
+          let cardSearch = document.getElementById("formSearch")
+            let search = new FormData(cardSearch)
+            fetch(this.apiURL, {
+                method: 'GET',
+                body: search,
+                mode: 'no-cors',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => {
+                return response.json();
+            })
+            .catch(err => {
+                err
+            });
+            this.$modal.hide('SearchCard');
+            this.search = '';
+            this.showFormSearch = false;
+            alert('Search Succesful!');
+        }else{
+            alert('Your search has missing fields!');
+        }
+    })
+    },
+    
+    show(){
+            this.$modal.show('SearchCard');
+        },
+        hide(){
+            this.$modal.hide('SearchCard');
         }
     }
-}
+    }
 </script>
+
+    
+    
+    
+    
