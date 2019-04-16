274ca9f5a72ae3d4f5d649748b5e3b677de21b94
Deawna Alfonsi <Deawna@D-Fit.com>
2019-04-16 12:18:03 -0400

    working on StudySession component

--- flashcards/src/components/StudySession.vue
+++ flashcards/src/components/StudySession.vue
@@ -1,17 +1,84 @@
 <template>
+<div>
     <h2>This is the start of the Study Session</h2>
+
+    <button id="start-study-session" v-on:click.prevent="show()">Start Study Session</button>
+
+    <modal id="study-session-form" name="startStudySession" :width="600" :height="225">
+        <div id="modal-header">
+            <h2>Choose A Deck</h2>
+        </div>
+            <div id="modal-body">  
+                <form id="formCreateDeck">
+                    <div id="deck-options">
+                        <span id="decks">Choose the deck for your study session:</span>
+                        <select id="user-decks" v-validate="'required|min_value:1'" v-model="category_id" name="user-deck-options">
+                            <option disabled value=0>Please Select a Deck</option>
+                            <option v-for="deck in userDecks" v-bind:key="deck.deckId" :value="deck.deckId"> {{deck.name}} </option>     
+                        </select>
+                        <span>{{ errors.first('user-deck-options') }}</span>
+                    </div>
+                    <div id=buttons>
+                        <button id="submitLoginButton" v-on:click.prevent="getDeck">Submit</button>
+                        <button id="cancelLoginButton" v-on:click.prevent="hide()">Cancel</button>
+                    </div>   
+                </form> 
+        </div>
+    </modal>
+
+
+
+</div>
 </template>
 
 <script>
 export default {
-    
+    beforeCreate(){
+        fetch("https://localhost:44337/api/deck/" + this.user.Person_id,{
+                method: 'GET'
+                })
+                .then(response => {
+                    return response.json();
+                })
+                 .then(data => {
+                    this.userDecks = data;
+                })
+                .catch(e => console.log(e));
+    },
+
     name: 'StudySession',
+
+    props: {
+        user: {
+            Person_id: 0,
+            Username: '',
+            FirstName: '',
+            LastName: '',
+            Password: '',
+            Hash: ''
+        }
+    },
 
     data () {
         return {
+            userId: 0,
+            deckId: 0,
+            name: '',
+            userDecks: [],
             apiURL: "https://localhost:44337/api/deck"
         }
+    },
+
+    methods: {
+         
+        show(){
+             this.$modal.show('startStudySession');
+        },
+        hide(){
+             this.$modal.hide('startStudySession');
+        }
     }
+
 }
 </script>
 
